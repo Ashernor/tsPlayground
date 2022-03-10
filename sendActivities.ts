266886e050b9f6553ca2ce822fe920fa5d3da1e0
Thomas ROBIN <ashernor@gmail.com>
2022-03-10 14:05:21 +0100

    [UPDATE] 30s wait

--- sendActivities.ts
+++ sendActivities.ts
@@ -13,15 +13,21 @@ import { thTenants } from './files/ck-th/TH_tenants';
 //   const thActivitySender = new sender('./files/ck-th/TH_sample.csv', 'view', tenant.trackerKey, ';');
 //   thActivitySender.sendActivities();
 // });
-  
+
 //const ckActivitySender = new sender('./files/ck-th/CK_sample.csv', 'view', ckTenants[0].trackerKey, ';');
 //ckActivitySender.sendActivities();
 
+const timer = ms => new Promise(res => setTimeout(res, ms))
 
-const dir = fs.opendirSync('files/ck-th/data/')
-let dirent
-while ((dirent = dir.readSync()) !== null) {
-  const ckActivitySender = new sender(`files/ck-th/data/${dirent.name}`, 'view', ckTenants[0].trackerKey, ';');
-  ckActivitySender.sendActivities();
+async function load() {
+  const dir = fs.opendirSync('files/ck-th/data/')
+  let dirent
+  while ((dirent = dir.readSync()) !== null) {
+    const ckActivitySender = new sender(`files/ck-th/data/${dirent.name}`, 'view', ckTenants[0].trackerKey, ';');
+    ckActivitySender.sendActivities();
+    console.log(dirent)
+    await timer(30000); // then the created Promise can be awaited
+  }
+  dir.closeSync()
 }
-dir.closeSync()
+load();
